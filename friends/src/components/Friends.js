df7bb9b5e114efb4bef264e97cbcaf8f67f80131
donavynhaley@gmail.com <donavynhaley@gmail.com>
2020-09-15 13:06:12 -0700

    Friends compnent now makes a git request with token to recive and display list of friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   Button,
@@ -7,20 +7,38 @@ import {
   CardTitle,
   CardText,
 } from "reactstrap";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Friends = (props) => {
+  const [freindsList, setFriendsList] = useState([]);
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/friends")
+      .then((res) => {
+        setFriendsList(res.data);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  if (freindsList.length == 0) {
+    return <h4>You have no Friends</h4>;
+  }
   return (
-    <div>
-      <Card body inverse color="primary">
-        <CardHeader>Rachel Green</CardHeader>
-        <CardBody>
-          <CardText>Age: 30 </CardText>
-          <CardText>Email: rachel@friends.com</CardText>
-          <Button size="lg" color="success">
-            Message
-          </Button>
-        </CardBody>
-      </Card>
+    <div className="friends">
+      {freindsList.map((friend) => {
+        return (
+          <Card body inverse color="primary" key={friend.id}>
+            <CardHeader>{friend.name}</CardHeader>
+            <CardBody>
+              <CardText>Age: {friend.age} </CardText>
+              <CardText>Email: {friend.email}</CardText>
+              <Button size="lg" color="success">
+                Message
+              </Button>
+            </CardBody>
+          </Card>
+        );
+      })}
     </div>
   );
 };
